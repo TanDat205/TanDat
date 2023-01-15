18ac72f19159c813f2d1320cb69f7aa765944ce2
HoTanDat64 <tandathoqt@gmail.com>
2023-01-15 21:05:21 +0700

    Update register.js

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 function giai(){
-  let a = document.getElementById("Register").value;
-  let b = document.getElementById("Age").value;
-  let c = document.getElementById("Phone").value;
+  let a = document.getElementById("name").value;
+  let b = document.getElementById("email").value;
+  let c = document.getElementById("password").value;
 
   if (isNaN(a) || isNaN(b)|| isNaN(c)){
       alert(" Vui lòng nhập số");
